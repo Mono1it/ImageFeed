2d9592000d49f356dfd7a612aa37230537a4512a
mono1it <host1425@gmail.com>
2025-05-14 15:15:46 +0300

    Реализовал scrollView без алгоритма рескейла изображения

--- ImageFeed/Presentation/ImagesList/SingleImageViewController.swift
+++ ImageFeed/Presentation/ImagesList/SingleImageViewController.swift
@@ -15,10 +15,26 @@ final class SingleImageViewController: UIViewController {
         }
     }
     
+
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak private var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        if let image = image {
+                imageView.frame.size = image.size
+            }
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
